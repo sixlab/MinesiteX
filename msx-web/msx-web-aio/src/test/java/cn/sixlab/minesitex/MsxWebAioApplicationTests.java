db4813508cce1109d65c973c260e3007df5bafe8
Patrick <nianqinianyi@163.com>
2017-11-07 15:29:06 +0800

    zuul 拆分一下

--- msx-web/msx-web-aio/src/test/java/cn/sixlab/minesitex/MsxWebAioApplicationTests.java
+++ msx-web/msx-web-aio/src/test/java/cn/sixlab/minesitex/MsxWebAioApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MsxPluginWxApplicationTests {
+public class MsxWebAioApplicationTests {
     
     @Test
     public void contextLoads() {
