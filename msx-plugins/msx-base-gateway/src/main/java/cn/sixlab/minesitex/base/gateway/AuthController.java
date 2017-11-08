cc2441296d741203570506aa7efa3a238046ec7e
Patrick <nianqinianyi@163.com>
2017-11-08 16:01:48 +0800

    修复 bug，添加首页

--- msx-plugins/msx-base-gateway/src/main/java/cn/sixlab/minesitex/base/gateway/AuthController.java
+++ msx-plugins/msx-base-gateway/src/main/java/cn/sixlab/minesitex/base/gateway/AuthController.java
@@ -1,50 +1,50 @@
-/**
- * Copyright (c) 2017 Sixlab. All rights reserved.
- * <p>
- * Under the GPLv3(AKA GNU GENERAL PUBLIC LICENSE Version 3).
- * see http://www.gnu.org/licenses/gpl-3.0-standalone.html
- * <p>
- * For more information, please see
- * https://sixlab.cn/
- *
- * @time: 2017/11/1 22:06
- * @author: Patrick <root@sixlab.cn>
- */
-package cn.sixlab.minesitex.base.gateway;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-
-@RestController
-public class AuthController {
-    
-    @Autowired
-    private AuthServiceImpl authService;
-    
-    @RequestMapping(value = "auth", method = RequestMethod.GET)
-    public ResponseEntity<?> createAuthenticationToken(String username, String password)
-            throws AuthenticationException {
-        final String token = authService.login(username, password);
-        
-        // Return the token
-        return ResponseEntity.ok("a");
-    }
-    
-    @RequestMapping(value = "refresh", method = RequestMethod.GET)
-    public ResponseEntity<?> refreshAndGetAuthenticationToken(
-            HttpServletRequest request) throws AuthenticationException {
-        String token = request.getHeader("Authorization");
-        String refreshedToken = authService.refresh(token);
-        if (refreshedToken == null) {
-            return ResponseEntity.badRequest().body(null);
-        } else {
-            return ResponseEntity.ok("b");
-        }
-    }
-}
+///**
+// * Copyright (c) 2017 Sixlab. All rights reserved.
+// * <p>
+// * Under the GPLv3(AKA GNU GENERAL PUBLIC LICENSE Version 3).
+// * see http://www.gnu.org/licenses/gpl-3.0-standalone.html
+// * <p>
+// * For more information, please see
+// * https://sixlab.cn/
+// *
+// * @time: 2017/11/1 22:06
+// * @author: Patrick <root@sixlab.cn>
+// */
+//package cn.sixlab.minesitex.base.gateway;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.servlet.http.HttpServletRequest;
+//
+//@RestController
+//public class AuthController {
+//
+//    @Autowired
+//    private AuthServiceImpl authService;
+//
+//    @RequestMapping(value = "auth", method = RequestMethod.GET)
+//    public ResponseEntity<?> createAuthenticationToken(String username, String password)
+//            throws AuthenticationException {
+//        final String token = authService.login(username, password);
+//
+//        // Return the token
+//        return ResponseEntity.ok("a");
+//    }
+//
+//    @RequestMapping(value = "refresh", method = RequestMethod.GET)
+//    public ResponseEntity<?> refreshAndGetAuthenticationToken(
+//            HttpServletRequest request) throws AuthenticationException {
+//        String token = request.getHeader("Authorization");
+//        String refreshedToken = authService.refresh(token);
+//        if (refreshedToken == null) {
+//            return ResponseEntity.badRequest().body(null);
+//        } else {
+//            return ResponseEntity.ok("b");
+//        }
+//    }
+//}
--- msx-plugins/msx-base-gateway/src/main/java/cn/sixlab/minesitex/base/gateway/AuthServiceImpl.java
+++ msx-plugins/msx-base-gateway/src/main/java/cn/sixlab/minesitex/base/gateway/AuthServiceImpl.java
@@ -1,42 +1,42 @@
-/**
- * Copyright (c) 2017 Sixlab. All rights reserved.
- * <p>
- * Under the GPLv3(AKA GNU GENERAL PUBLIC LICENSE Version 3).
- * see http://www.gnu.org/licenses/gpl-3.0-standalone.html
- * <p>
- * For more information, please see
- * https://sixlab.cn/
- *
- * @time: 2017/11/1 22:06
- * @author: Patrick <root@sixlab.cn>
- */
-package cn.sixlab.minesitex.base.gateway;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AuthServiceImpl {
-    
-    public String login(String username, String password) {
-        //UsernamePasswordAuthenticationToken upToken = new UsernamePasswordAuthenticationToken(username, password);
-        //// Perform the security
-        //final Authentication authentication = authenticationManager.authenticate(upToken);
-        //SecurityContextHolder.getContext().setAuthentication(authentication);
-        //
-        //// Reload password post-security so we can generate token
-        //final UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        //final String token = jwtTokenUtil.generateToken(userDetails);
-        //return token;
-        return null;
-    }
-    
-    public String refresh(String oldToken) {
-        //final String token = oldToken.substring(tokenHead.length());
-        //String username = jwtTokenUtil.getUsernameFromToken(token);
-        //JwtUser user = (JwtUser) userDetailsService.loadUserByUsername(username);
-        //if (jwtTokenUtil.canTokenBeRefreshed(token, user.getLastPasswordResetDate())) {
-        //    return jwtTokenUtil.refreshToken(token);
-        //}
-        return null;
-    }
-}
+///**
+// * Copyright (c) 2017 Sixlab. All rights reserved.
+// * <p>
+// * Under the GPLv3(AKA GNU GENERAL PUBLIC LICENSE Version 3).
+// * see http://www.gnu.org/licenses/gpl-3.0-standalone.html
+// * <p>
+// * For more information, please see
+// * https://sixlab.cn/
+// *
+// * @time: 2017/11/1 22:06
+// * @author: Patrick <root@sixlab.cn>
+// */
+//package cn.sixlab.minesitex.base.gateway;
+//
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class AuthServiceImpl {
+//
+//    public String login(String username, String password) {
+//        //UsernamePasswordAuthenticationToken upToken = new UsernamePasswordAuthenticationToken(username, password);
+//        //// Perform the security
+//        //final Authentication authentication = authenticationManager.authenticate(upToken);
+//        //SecurityContextHolder.getContext().setAuthentication(authentication);
+//        //
+//        //// Reload password post-security so we can generate token
+//        //final UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+//        //final String token = jwtTokenUtil.generateToken(userDetails);
+//        //return token;
+//        return null;
+//    }
+//
+//    public String refresh(String oldToken) {
+//        //final String token = oldToken.substring(tokenHead.length());
+//        //String username = jwtTokenUtil.getUsernameFromToken(token);
+//        //JwtUser user = (JwtUser) userDetailsService.loadUserByUsername(username);
+//        //if (jwtTokenUtil.canTokenBeRefreshed(token, user.getLastPasswordResetDate())) {
+//        //    return jwtTokenUtil.refreshToken(token);
+//        //}
+//        return null;
+//    }
+//}
