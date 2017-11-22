94514531aa3303287b1b4762fed042dcca2e1880
Patrick <nianqinianyi@163.com>
2017-11-22 11:53:40 +0800

    用代码生成器生成一个试一试

---
+++ msx-plugins/msx-base-gateway/src/main/java/cn/sixlab/minesitex/base/gateway/JwtUtil.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2017 Sixlab. All rights reserved.
+ * <p>
+ * Under the GPLv3(AKA GNU GENERAL PUBLIC LICENSE Version 3).
+ * see http://www.gnu.org/licenses/gpl-3.0-standalone.html
+ * <p>
+ * For more information, please see
+ * https://sixlab.cn/
+ *
+ * @time: 2017/11/8 17:17
+ * @author: Patrick <root@sixlab.cn>
+ */
+package cn.sixlab.minesitex.base.gateway;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtUtil {
+    public static void main(String[] args) {
+        Date date = new Date(new Date().getTime() + 3024000000l);
+        String compact = "Bearer "+Jwts.builder()
+                .setSubject("nianqinianyi")
+                .setExpiration(date)
+                .signWith(SignatureAlgorithm.HS512, "Sixlab's MinesiteX")
+                .compact();
+    //Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJuaWFucWluaWFueWkiLCJleHAiOjE1MTYxODA3ODN9.bRTUeZvQdrn1bdV8LMJ5hpdUAqOa7wGpgdXS-67ai2bZsFGyoMcUiCuiDVdcsLmov5347E9ShBu_iSbxnKbujA
+    //Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJuaWFucWluaWFueWkiLCJleHAiOjE1MTMxNTcwMDR9.PhnpYCUsmMet84LTrhfdRbdoJV24u7wbNUEFedR4QDBmdDI8HqP4uLUsS5JIx_zSzFijm0DYK80Yyey7c2RjQQ
+        System.out.println(date);
+        System.out.println(compact);
+    }
+}
