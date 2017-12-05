cf87f0d59892f0fa112b4ac3442596db9c9962a8
Patrick <nianqinianyi@163.com>
2017-12-05 14:30:24 +0800

    更换为 MIT 协议

--- msx-libs/msx-lib-base/src/main/java/cn/sixlab/minesitex/lib/base/util/InputStreamUtil.java
+++ msx-libs/msx-lib-base/src/main/java/cn/sixlab/minesitex/lib/base/util/InputStreamUtil.java
@@ -1,9 +1,8 @@
 /**
  * Copyright (c) 2017 Sixlab. All rights reserved.
- *
- * Under the GPLv3(AKA GNU GENERAL PUBLIC LICENSE Version 3).
- * see http://www.gnu.org/licenses/gpl-3.0-standalone.html
- *
+ * <p>
+ * License information see the LICENSE file in the project's root directory.
+ * <p>
  * For more information, please see
  * https://sixlab.cn/
  *
--- msx-libs/msx-lib-base/src/main/java/cn/sixlab/minesitex/lib/base/util/JsonUtl.java
+++ msx-libs/msx-lib-base/src/main/java/cn/sixlab/minesitex/lib/base/util/JsonUtl.java
@@ -1,8 +1,7 @@
 /**
  * Copyright (c) 2017 Sixlab. All rights reserved.
  * <p>
- * Under the GPLv3(AKA GNU GENERAL PUBLIC LICENSE Version 3).
- * see http://www.gnu.org/licenses/gpl-3.0-standalone.html
+ * License information see the LICENSE file in the project's root directory.
  * <p>
  * For more information, please see
  * https://sixlab.cn/
